b922828e4160f67d00e024eaa0dfefc52bc35e77
rat535 <ratnesht535@gmail.com>
2025-02-28 12:24:04 +0530

    API

--- src/services/ApplicantAPIService.js
+++ src/services/ApplicantAPIService.js
@@ -5,9 +5,9 @@ import axios from 'axios';
 
 
     
-    //export const apiUrl = 'http://localhost:8081';
+    export const apiUrl = 'http://192.168.86.29:8081';
     //export const apiUrl = 'https://kqryamxpv3.ap-south-1.awsapprunner.com';
-     export const apiUrl = 'https://g23jza8mtp.ap-south-1.awsapprunner.com';
+    //  export const apiUrl = 'https://g23jza8mtp.ap-south-1.awsapprunner.com';
 
 
 const ApplicantAPIService = {
